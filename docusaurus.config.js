ab2ab5706ff37b974ecb89be305e7ae865e21fd0
nkondratyk <nkondratyk@playtika.com>
2022-09-11 19:40:10 +0300

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Harpia Documentation',
   tagline: 'IT Incident management system',
-  url: 'https://github.com',
+  url: 'https://docs.harpia.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
